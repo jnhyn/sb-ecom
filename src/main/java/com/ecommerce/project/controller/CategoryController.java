c2d9b69503a4711272fe04778e3f6f9bcdab66bb
jnhyn <jnhyn.lee@gmail.com>
2026-01-21 22:51:41 +0900

    RequestParam 작업

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +23,12 @@ public class CategoryController {
 
   @Autowired
   private CategoryService categoryService;
+
+
+  @GetMapping("/echo")
+  public ResponseEntity<String> echoMessage(@RequestParam(name = "message") String message) {
+    return new ResponseEntity<>("Hello World : " + message, HttpStatus.OK);
+  }
 
   @GetMapping("/public/categories")
   public ResponseEntity<CategoryResponse> getAllCategories() {
